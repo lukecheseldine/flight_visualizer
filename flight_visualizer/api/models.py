bcde9f616586540e261ef85bc777b4571d79fb8b
Daniel Qiu <qiudaniel03@gmail.com>
2022-09-05 17:05:01 -0400

    start storing api data into django
    Currently, it makes an api call to all and then, it would get the time (which defaults to current time), as well as the first flights callsign and stores it into the db

--- flight_visualizer/api/models.py
+++ flight_visualizer/api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import requests, json
 
 # Create your models here.
+# class flightDataManager(models.Manager):
+#     def add_flight(self, time, callsign):
+#         flight = self.create(time = time, callsign = callsign)
+#         return flight
+class flightData(models.Model):
+    time = models.CharField(max_length=100)
+    callsign = models.CharField(max_length=100)
+    # objects = flightDataManager()
+API = "https://opensky-network.org/api/states/all"
+result = requests.get(API)
+data = result.json()
+ftime = data.get("time")
+callsign = data.get("states")[0][1]
+print(ftime, callsign)
+flight = flightData.objects.create(time = ftime, callsign = callsign)
+flight.save()
