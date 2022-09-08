709ab5fb9eabb387d2ba939d0b2e688878a3605b
Samuel Liu <liusamuelt@gmail.com>
2022-09-08 00:16:17 -0400

    database functional

--- flight_visualizer/api/models.py
+++ flight_visualizer/api/models.py
@@ -1,28 +1,36 @@
+from operator import mod
 from sys import maxsize
 from django.db import models
 import requests
 import json 
 
-class flightData(models.Model):
-    origin = models.CharField(max_length=30)
-    destination = models.CharField(max_length=30)
-    callsign = models.CharField(max_length=20)
-    departure_time = models.DateTimeField()
-    arrival_time = models.DateTimeField()
-    carbon_emissions = models.IntegerField()
+class FlightData(models.Model):
+    time = models.CharField(max_length=20, default=0)
+    callsign = models.CharField(max_length=20, default=0)
+    lat = models.CharField(max_length=20, default=0)
+    long = models.CharField(max_length=20, default=0)
+    velocity = models.CharField(max_length=20, default=0)
+    true_track = models.CharField(max_length=20, default=0)
+    carbon = models.CharField(max_length=20, default=0)
 
 path = "https://opensky-network.org/api/states/all"
 
-data = requests.get(path)
-data = data.json()
-
-
+data1 = requests.get(path)
+data = data1.json()
+states = data.get("states")
 departure_time = data.get("time")
-callsign = data.get("states")[0][1]
 
-for i in range(len(data.get("states"))):
-    flight = flightData.objects.create(time = departure_time, callsign = data[i][1])
-    flight.save()
+# flight_data = FlightData.objects.create(time = departure_time, callsign = states[0][1], 
+# lat = states[0][6], long = states[0][5], velocity = states[0][9], true_track = states[0][10], carbon = 0)
+# flight_data.save()
+
+
+
+# for i, flight in enumerate(states):
+#     if i == 1000:
+#         break
+#     flightdata = FlightData.objects.create(time = departure_time, callsign = flight[1])
+#     flightdata.save()
 
 
 
--- flight_visualizer/flight_visualizer/settings.py
+++ flight_visualizer/flight_visualizer/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'flight_visualizer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'flight_data',
+        'USER': 'postgres',
+        'PASSWORD': 'Password12345',
+        'HOST': 'localhost', 
+        'PORT': '5432'
     }
 }
 
